b9363b4e0da50a2a4c5023586b84c80780744111
Chris Cox <chris.cox@abandonedfactory.net>
2016-10-05 09:47:01 -0700

    Return a JSON object describing the app health

--- app/controllers/service_status_controller.rb
+++ app/controllers/service_status_controller.rb
@@ -2,13 +2,13 @@ require 'time'
 class ServiceStatusController < ApplicationController
   def index
     query_start = Time.now
-    @products = Products.all
+    @products = Product.all
     @query_timing = ((Time.now.to_f - query_start.to_f) * 1000).round
     @qty_found = @products.length 
 
     render :json => {
 	:product_count => @qty_found,
-	:mysql_response_ms => @query_timing,
+	:db_response_ms => @query_timing,
     }
   end
 end
