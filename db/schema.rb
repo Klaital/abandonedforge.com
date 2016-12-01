25455407d31c554d668591b3ac5954f5fc8f44e0
Chris Cox <chris.cox@abandonedfactory.net>
2016-12-01 12:07:06 -0800

    Adding slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201183619) do
+ActiveRecord::Schema.define(version: 20161201195955) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161201183619) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "slug"
+    t.index ["slug"], name: "index_products_on_slug"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
