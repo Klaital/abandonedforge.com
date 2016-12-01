ed5c12bd25da312f3760f83620fadb9e1d586caa
Chris Cox <chris.cox@abandonedfactory.net>
2016-12-01 13:35:09 -0800

    Switching to responsive tiled layout for product index

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,6 +16,21 @@ class Product < ApplicationRecord
     small_dimensions
   end
 
+  # Computes a new height x width string after scaling the main image's
+  # width to match the specified height, while keeping the correct aspect ratio.
+  def image_resize(new_height= 300)
+    if product_image_url.empty? || product_image_size.empty?
+      return "#{new_height}x#{new_height}"
+    end
+
+    dimensions = product_image_size.split('x')
+    # For a list view, we want a thumbnail with a vertical size of 32px, with the horizontal scaled accordingly.
+    scale = dimensions[1].to_f / new_height.to_f
+    small_dimensions = dimensions.map {|d| (d.to_i / scale).floor}.join('x')
+    logger.debug("Modifying dimensions (#{product_image_size}): #{dimensions} / #{scale} = #{small_dimensions}")
+    small_dimensions
+  end
+
   def description_shortened(length = 30)
     return description if description.length <= length
     description[0...length] + '...'
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#about'
+  root 'products#index'
 
   # Static Pages
   get '/about', to: 'pages#about'
